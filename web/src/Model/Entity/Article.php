c993551eac61a4246e5f5891e091a2dfbc894401
tkzw_yuki <gulgul.groove@gmail.com>
2020-05-15 10:32:38 +0900

    [update] tagの改良

--- web/src/Model/Entity/Article.php
+++ web/src/Model/Entity/Article.php
@@ -3,6 +3,7 @@
 namespace App\Model\Entity;
 
 use Cake\ORM\Entity;
+use Cake\Collection\Collection;
 
 class Article extends Entity
 {
@@ -11,5 +12,21 @@ class Article extends Entity
         'id' => false,
         'slug' => false,
     ];
+
+    protected function _getTagString()
+    {
+        if (isset($this->_fields['tag_string'])) {
+            return $this->_fields['tag_string'];
+        }
+        if (empty($this->tags)) {
+            return '';
+        }
+
+        $tags = new Collection($this->tags);
+        $str = $tags->reduce(function ($string, $tags) {
+            return $string . $tags->title . ', ';
+        }, '');
+        return trim($str, ', ');
+    }
 }
 ?>
